ca6d9706f76112a089721ef499d935f2d39535a3
Maria Luísa <malumaricatoo@gmail.com>
2021-05-05 17:54:06 -0300

    Lista 2

--- java/AULAS/src/lista2/tarefa4.java
+++ java/AULAS/src/lista2/tarefa4.java
@@ -11,19 +11,31 @@ public class tarefa4 {
 		
 		//variaveis
 		double numero = 0.0;
-		double par = 0.0;
-		double impar = 0.0;
-				
+		double numeroFinal = 0.0;
+		
 		//entrada
 		System.out.println("Digite um número: ");
 		numero = leia.nextDouble();
 		
 		//processamento
-		if (numero%2 == 0) {
-			
-			
+		if ((numero%2) == 0) {
+			numeroFinal = Math.sqrt(numero);	
 			
 		}
+		else if ((numero%2) != 0) {
+			numeroFinal = Math.pow((numero), 2);
+			
+		}
+		//saida
+		
+		System.out.printf("O número final é: %.2f",numeroFinal);
+		
+		
+		/*
+		 * System.out.println(((valor%2)==0) ? "NUMERO PAR": "NUMERO IMPAR");
+		 * System.out.println((valor==0)? "ZERO É NEUTRO":((valor%2)==0) ? "NUMERO PAR": "NUMERO IMPAR");
+
+		 */
 		
 
 	}
